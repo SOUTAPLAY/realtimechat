6bc362fcb61011fa785024396276ba82892f408f
SOUTAPLAY <97559316+SOUTAPLAY@users.noreply.github.com>
2024-11-11 23:08:24 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// Cache name
+const CACHE_NAME = 'pwa-sample-caches-v1';
+// Cache targets
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './index.js',
+  './src/favicon.ico',
+  './src/icon-512×512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
